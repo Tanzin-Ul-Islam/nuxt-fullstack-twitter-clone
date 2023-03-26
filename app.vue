c7ba120704bc4d858b8a4e9f69adf6be51a57491
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2023-03-27 01:53:45 +0600

    Project init

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- left sidebar -->
+
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+
+          <!-- main content -->
+
+          <!-- right sidebar -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const darkMode = ref(true);
+    return {
+      darkMode,
+    };
+  },
+};
+</script>
---
+++ components/sidebar/Left/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min" :class="defaultTransition">
+      <nuxt-link to="/">
+        <div class="h-8 w-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+    <SidebarLeftTab>
+      
+    </SidebarLeftTab>
+  </div>
+</template>
+
+<script>
+import { HomeIcon  } from '@heroicons/vue/24/solid'
+export default {
+  components: {
+    HomeIcon ,
+  },
+  setup() {
+    const { defaultTransition } = useTailwindConfig();
+    return {
+      defaultTransition,
+    };
+  },
+};
+</script>
